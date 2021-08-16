71287de622b1d4caf18e41c197d3c970ca638f02
sgtroberti <guimourinha@gmail.com>
2021-08-16 20:27:13 -0300

    add pagina quem_somos

---
+++ pages/quem_somos.php
@@ -0,0 +1,35 @@
+<?php
+require_once("../../21146657-5_GuilhermeRoberti/pages/header.php");
+?>
+<div class="container centralizar quem_somos">
+    <br>
+    <h3>Quem somos!</h3>
+    <hr>
+    <h6>Somos uma empresa familiar que acredita que todos os bons momentos precisam ser marcados com um bolo!</h6>
+    <hr>
+    <div class="mapouter">
+        <div class="gmap_canvas mapa"><iframe width="200" height="150" id="gmap_canvas" src="https://maps.google.com/maps?q=unicesumar,%20maring%C3%A1&t=&z=9&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://fmovies-online.net">fmovies</a><br>
+            <style>
+                .mapouter {
+                    position: relative;
+                    text-align: right;
+                    height: 150px;
+                    width: 200px;
+                }
+            </style><a href="https://www.embedgooglemap.net"></a>
+            <style>
+                .gmap_canvas {
+                    overflow: hidden;
+                    background: none !important;
+                    height: 150px;
+                    width: 200px;
+                }
+            </style>
+        </div>
+    </div>
+</div>
+
+
+<?php
+require_once("../../21146657-5_GuilhermeRoberti/pages/footer.php");
+?>
